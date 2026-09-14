2948ea2f2912debe36e7225315825b912f8ca7dc
Vaulta <vaulta@local.dev>
2026-09-14 18:09:05 +0530

    updated header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -56,7 +56,7 @@ export function Header({
           </Link>
         </div>
 
-        <div className="flex items-center gap-1 shrink-0">
+        <div className="flex items-center gap-2 shrink-0">
           {showThemeSwitcher && <ThemeSwitcher />}
           {actions}
         </div>
